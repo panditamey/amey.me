ef91164a3cb907872d05ffc5b6a9b391f7998a4d
GDSC-ACE <gdscace@gmail.com>
2022-12-19 19:01:50 +0530

    Added Link to Resume Button

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,12 @@ import NavBar from "./NavBar";
 import Spline from '@splinetool/react-spline';
 import Typed from 'react-typed';
 
+
 function Hero() {
+
+    function btnClick() {
+        window.open("https://www.google.com");
+    }
     return (
         <>
             <NavBar />
@@ -13,13 +18,14 @@ function Hero() {
                 </div>
                 <div className="xl:w-[40%] text-left sm:w-full">
                     <div className='pl-4 text-3xl font-bold flex'>
-                        <Typed 
-                        className='xl:text-6xl sm:text-1xl font-bold text-center pl-[17%] pb-9 text-white' strings={["I'm Amey Pandit", "I'm a Student", "I'm an App Developer"]} typeSpeed={120} backSpeed={140} loop smartBackspace/>
-                        
+                        <Typed
+                            className='xl:text-6xl sm:text-1xl font-bold text-center pl-[17%] pb-9 text-white' strings={["I'm Amey Pandit", "I'm a Student", "I'm an App Developer"]} typeSpeed={120} backSpeed={140} loop smartBackspace />
+
                     </div>
                     <div className='text-center'>
-
-                    <button className='bg-[#4a15e8] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-white'>Resume</button>
+                    <a href="https://google.com" target="_blank" rel="noreferrer">
+                            <button className='bg-[#4a15e8] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-white'>Resume</button>
+                            </a>
                     </div>
                 </div>
             </div>
