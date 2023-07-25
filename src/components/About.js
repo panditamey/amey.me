9832db40093a531b30db5c032202c767c44399a1
Amey Pandit <84444050+panditamey@users.noreply.github.com>
2023-07-25 16:58:43 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 function About() {
     return (
-        <div className='h-auto  justify-center text-center text-white'>
+        <div className='h-auto invisible justify-center text-center text-white'>
             <div>
                 <h1 className='text-4xl font-bold py-6 uppercase'>About</h1>
                 <p class="text-clip overflow-hidden lg:text-lg sm:text-xs md:text-sm text-xs xl:px-40 sm:px-20 md:px-20">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
